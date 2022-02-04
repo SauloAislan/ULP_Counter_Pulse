071ba9411d1c361a77ce231b12ff9af6786c843e
SauloAislan <sauloaislan20@gmail.com>
2022-02-04 18:50:36 -0300

    init

---
+++ ulp_main.h
@@ -0,0 +1,9 @@
+#include "Arduino.h"
+
+extern uint32_t ulp_entry;
+extern uint32_t ulp_debounce_counter;
+extern uint32_t ulp_debounce_max_count;
+extern uint32_t ulp_next_edge;
+extern uint32_t ulp_io_number;
+extern uint32_t ulp_edge_count_to_wake_up;
+extern uint32_t ulp_edge_count;
